69d8d20512a59fd247ce48bb7153c10166c8e5c6
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-12-24 13:08:08 +0530

    while and do-while loop

---
+++ 11_Loops/main (1).c
@@ -0,0 +1,51 @@
+/*
+while loop:
+while(condition){
+    statement
+    
+    increment/decrement
+}
+*/
+
+// #include <stdio.h>
+// int main(){
+//     int i=11;
+    
+//     while(i<=10){
+//         printf("Hello Aliens.\n");
+        
+//         i++;
+//     }
+// }
+
+
+/*
+do-while:
+if condition is true then loop will continue but even if it is false it will iterate once.
+
+do{
+    statement..
+    
+    increment/decrement
+}while(condition)
+
+
+*/
+
+#include <stdio.h>
+int main(){
+    int i=1;
+    do{
+        printf("%d.Hello Aliens.\n",i);
+        i++;
+    }while(i<=10);
+}
+
+
+
+
+
+
+
+
+
