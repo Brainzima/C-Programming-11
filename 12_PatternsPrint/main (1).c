19e2143addaf8357a34c1ccf1dfd57025eda71f7
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-12-25 12:39:01 +0530

    patterns print

---
+++ 12_PatternsPrint/main (1).c
@@ -0,0 +1,93 @@
+/*
+pattern print 
+
+*
+**
+***
+****
+*****
+
+*/
+
+// #include<stdio.h>
+// int main(){
+    
+//     for(int i=1; i<=15; i++){
+        
+//         for(int j=1; j<=i; j++){
+//             printf("*");
+//         }
+        
+//         printf("\n");
+//     }
+// }
+
+
+
+/*
+pattern print 
+
+1 
+22 
+333
+4444
+55555
+
+*/
+
+// #include<stdio.h>
+// int main(){
+    
+//     for(int i=1; i<=5; i++){
+        
+//         for(int j=1; j<=i; j++){
+//             printf("%d",i);
+//         }
+        
+//         printf("\n");
+//     }
+// }
+
+/*
+pattern print 
+
+1 
+12 
+123
+1234
+12345
+
+*/
+
+#include<stdio.h>
+int main(){
+    
+    for(int i=1; i<=5; i++){
+        
+        for(int j=1; j<=i; j++){
+            printf("%d ",j);
+        }
+        
+        printf("\n");
+    }
+}
+
+
+
+// Write a program to take user input of how many stars to be printed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
